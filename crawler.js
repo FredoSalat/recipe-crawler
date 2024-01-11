9978961ddf86810b55b07e5d9620be8e8e3be733
FredoSalat <fredrik.marthinsens@gmail.com>
2024-01-11 22:20:01 +0100

    fully working crawler for image, ingredients and title

--- crawler.js
+++ crawler.js
@@ -1,45 +1,40 @@
 import { PlaywrightCrawler } from "crawlee";
-import { sanitizeText } from "../js/utilities.js";
 import sqlite3 from "sqlite3";
+import { sanitizeText } from "./utilities.js";
 
 const db = new sqlite3.Database("recipe.db");
 
-const loadedCategoriesLimit = 1;
-const loadedRecipeLimit = 1;
+const loadedCategoriesLimit = 200;
+const loadedRecipeLimit = 2000;
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ page, request, enqueueLinks }) => {
     try {
       if (request.label === "DETAIL") {
+        // Title
         const titleElement = await page.locator(".c-recipe__title");
-
-        const imageElements = await page.$$(".c-recipe__image > picture > img");
-
-        if (!imageElements || imageElements.length === 0) {
-          throw new Error(`Image not found on this page ${request.url}`);
-        }
-
-        const ingredientElements = await page.$$(
-          ".recipe-page-body__ingredients"
-        );
-
-        const imageElement = imageElements[0];
 
         if (!titleElement) {
           throw new Error(`Title not found on this page ${request.url}`);
         }
 
+        const rawTitle = await titleElement.textContent();
+
+        const title = sanitizeText(rawTitle);
+
+        // Image
+
+        const imageElements = await page.$$(".c-recipe__image > picture > img");
+
+        if (!imageElements || imageElements.length === 0) {
+          throw new Error(`Images not found on this page ${request.url}`);
+        }
+
+        const imageElement = imageElements[0];
+
         if (!imageElement) {
           throw new Error(`Image not found on this page ${request.url}`);
         }
-
-        if (!ingredientElements) {
-          throw new Error(`Ingredients not found on this page ${request.url}`);
-        }
-
-        // recipe name
-        const rawTitle = await titleElement.textContent();
-        const title = sanitizeText(rawTitle);
 
         const rawImageURL = await imageElement.getAttribute("src");
 
@@ -51,6 +46,16 @@ const crawler = new PlaywrightCrawler({
 
         if (!imageURL) {
           throw new Error(`Image URL not found on this page ${request.url}`);
+        }
+
+        // Ingredients
+
+        const ingredientElements = await page.$$(
+          ".recipe-page-body__ingredients"
+        );
+
+        if (!ingredientElements) {
+          throw new Error(`Ingredients not found on this page ${request.url}`);
         }
 
         // Extracting ingredients
@@ -85,17 +90,26 @@ const crawler = new PlaywrightCrawler({
           ingredients,
         };
 
-        // Convert the ingredients object to a JSON string
         const ingredientsString = JSON.stringify(recipe.ingredients);
 
-        db.serialize(() => {
-          db.run(`
+        await new Promise((resolve, reject) => {
+          db.run(
+            `
             CREATE TABLE IF NOT EXISTS recipe (
               title TEXT,
               imageURL TEXT,
               ingredients TEXT
-            )
-          `);
+            )`,
+            (err) => {
+              if (err) {
+                console.error("Error creating table:", err.message);
+                reject(err);
+              } else {
+                console.log("Table 'recipe' created successfully");
+                resolve();
+              }
+            }
+          );
         });
 
         const stmt = db.prepare(
@@ -105,9 +119,6 @@ const crawler = new PlaywrightCrawler({
         stmt.run(recipe.title, recipe.imageURL, ingredientsString);
 
         stmt.finalize();
-
-        //console.log(recipe);
-        //await Dataset.pushData(recipe);
       } else if (request.label === "CATEGORY") {
         // Queues each recipe within every category
         await page.waitForSelector(".u-1\\/2 > a");
