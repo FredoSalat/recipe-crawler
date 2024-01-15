97ba9acd3beaa09c741da1f85402aba5f278ea5c
FredoSalat <fredrik.marthinsens@gmail.com>
2024-01-15 15:49:03 +0100

    refactoring

--- crawler.js
+++ crawler.js
@@ -1,87 +1,33 @@
 import { PlaywrightCrawler } from "crawlee";
 import sqlite3 from "sqlite3";
-import { sanitizeText } from "./utilities.js";
+import {
+  getImage,
+  getIngredients,
+  getTitle,
+  sanitizeText,
+} from "./utilities.js";
 
 const db = new sqlite3.Database("recipe.db");
 
-const loadedCategoriesLimit = 200;
-const loadedRecipeLimit = 2000;
+const loadedCategoriesLimit = 1;
+const loadedRecipeLimit = 2;
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ page, request, enqueueLinks }) => {
     try {
       if (request.label === "DETAIL") {
-        // Title
-        const titleElement = await page.locator(".c-recipe__title");
+        const title = await getTitle(page, ".c-recipe__title", request.url);
 
-        if (!titleElement) {
-          throw new Error(`Title not found on this page ${request.url}`);
-        }
-
-        const rawTitle = await titleElement.textContent();
-
-        const title = sanitizeText(rawTitle);
-
-        // Image
-
-        const imageElements = await page.$$(".c-recipe__image > picture > img");
-
-        if (!imageElements || imageElements.length === 0) {
-          throw new Error(`Images not found on this page ${request.url}`);
-        }
-
-        const imageElement = imageElements[0];
-
-        if (!imageElement) {
-          throw new Error(`Image not found on this page ${request.url}`);
-        }
-
-        const rawImageURL = await imageElement.getAttribute("src");
-
-        if (!rawImageURL) {
-          throw new Error("Raw Image URL not found on this page");
-        }
-
-        const imageURL = rawImageURL.split("?")[0];
-
-        if (!imageURL) {
-          throw new Error(`Image URL not found on this page ${request.url}`);
-        }
-
-        // Ingredients
-
-        const ingredientElements = await page.$$(
-          ".recipe-page-body__ingredients"
+        const imageURL = getImage(
+          page,
+          ".c-recipe__image > picture > img",
+          request.url
         );
 
-        if (!ingredientElements) {
-          throw new Error(`Ingredients not found on this page ${request.url}`);
-        }
-
-        // Extracting ingredients
-        const ingredients = await Promise.all(
-          ingredientElements.map(async (element, index) => {
-            const rawPreparation = await ingredientElements[
-              index
-            ].textContent();
-            const preparation = sanitizeText(rawPreparation);
-
-            // Extracting ingredient text content excluding <span> elements
-            const ingredientElement = await element.evaluate((tdElement) => {
-              const spanElements = tdElement.querySelectorAll("span");
-              spanElements.forEach((spanElement) => {
-                spanElement.remove();
-              });
-              return tdElement.textContent;
-            });
-
-            const ingredient = sanitizeText(ingredientElement);
-
-            return {
-              ingredient,
-              preparation,
-            };
-          })
+        const ingredients = getIngredients(
+          page,
+          ".recipe-page-body__ingredients",
+          request.url
         );
 
         const recipe = {
--- utilities.js
+++ utilities.js
@@ -7,3 +7,80 @@ export const sanitizeText = (text) => {
     .replace(/\s+/g, " ")
     .replace(/^[,\s]+|[,\s]+$/g, "");
 };
+
+export const getTitle = async (page, locator, requestURL) => {
+  console.log(locator);
+  const titleElement = await page.locator(locator);
+
+  if (!titleElement) {
+    throw new Error(`Title not found on this page ${requestURL}`);
+  }
+
+  const rawTitle = await titleElement.textContent();
+
+  const title = sanitizeText(rawTitle);
+
+  return title;
+};
+
+export const getImage = async (page, locator, requestURL) => {
+  const imageElements = await page.$$(locator);
+
+  if (!imageElements || imageElements.length === 0) {
+    throw new Error(`Images not found on this page ${requestURL}`);
+  }
+
+  const imageElement = imageElements[0];
+
+  if (!imageElement) {
+    throw new Error(`Image not found on this page ${requestURL}`);
+  }
+
+  const rawImageURL = await imageElement.getAttribute("src");
+
+  if (!rawImageURL) {
+    throw new Error("Raw Image URL not found on this page");
+  }
+
+  const imageURL = rawImageURL.split("?")[0];
+
+  if (!imageURL) {
+    throw new Error(`Image URL not found on this page ${requestURL}`);
+  }
+
+  return imageURL;
+};
+
+export const getIngredients = async (page, locator, requestURL) => {
+  const ingredientElements = await page.$$(locator);
+
+  if (!ingredientElements) {
+    throw new Error(`Ingredients not found on this page ${requestURL}`);
+  }
+
+  // Extracting ingredients
+  const ingredients = await Promise.all(
+    ingredientElements.map(async (element, index) => {
+      const rawPreparation = await ingredientElements[index].textContent();
+      const preparation = sanitizeText(rawPreparation);
+
+      // Extracting ingredient text content excluding <span> elements
+      const ingredientElement = await element.evaluate((tdElement) => {
+        const spanElements = tdElement.querySelectorAll("span");
+        spanElements.forEach((spanElement) => {
+          spanElement.remove();
+        });
+        return tdElement.textContent;
+      });
+
+      const ingredient = sanitizeText(ingredientElement);
+
+      return {
+        ingredient,
+        preparation,
+      };
+    })
+  );
+
+  return ingredients;
+};
