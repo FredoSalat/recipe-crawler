00acd6687cdbec5d31081f990ad6f57776edb804
FredoSalat <fredrik.marthinsens@gmail.com>
2024-01-30 22:38:22 +0100

    New and working locators for category and recipe

--- main.js
+++ main.js
@@ -11,44 +11,53 @@ import {
 const db = new sqlite3.Database("recipe.db");
 
 const loadedCategoriesLimit = 1;
-const loadedRecipeLimit = 2;
+const loadedRecipeLimit = 45;
+
+const recipeSelector = ".scaling-card-image";
+const nextButton = "";
+const categorySelector = ".relative > a";
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ page, request, enqueueLinks }) => {
     try {
       if (request.label === "DETAIL") {
+        console.log(`Recipe: ${request.url}`);
         const title = await getTitle(page, request.url);
         const imageURL = await getImage(page, request.url);
         const ingredients = await getIngredients(page, request.url);
         await addRecipeToDatabase(db, title, imageURL, ingredients);
       } else if (request.label === "CATEGORY") {
         // Queues each recipe within every category
-        await page.waitForSelector(".u-1\\/2 > a");
+
+        await page.waitForSelector(recipeSelector);
         await enqueueLinks({
           limit: loadedRecipeLimit,
-          selector: ".u-1\\/2 > a",
+          selector: recipeSelector,
           label: "DETAIL",
         });
 
         // Paginates through all recipe pages
-        const nextButton = await page.$("a.pagination__next");
+        const nextButton = await page.$(nextButton);
 
         if (nextButton) {
           await enqueueLinks({
-            selector: ".c-pagination-v2__next > a",
+            selector: nextButton,
             label: "CATEGORY",
           });
+          console.log("Next button clicked");
         }
-        console.log(request.url);
+        console.log(`Category: ${request.url}`);
       } else {
         // Queues all categories on the category page
-        await page.waitForSelector(".c-card > a");
+        await page.waitForSelector(categorySelector);
+
         await enqueueLinks({
           limit: loadedCategoriesLimit,
-          selector: ".c-card > a",
+          selector: categorySelector,
           label: "CATEGORY",
         });
-        console.log(request.url);
+
+        console.log(`List of categories: ${request.url}`);
       }
     } catch (error) {
       console.error("An error occurred:", error.message);
